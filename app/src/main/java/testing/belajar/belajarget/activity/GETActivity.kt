fff63347b18ea0222122ad2045426161203c5178
nabildjulfiansyah <muhnabildjulfiansyah@gmail.com>
2020-02-15 21:17:47 +0800

    get data from response and get key userId using getInt instead getJSONObject

--- app/src/main/java/testing/belajar/belajarget/activity/GETActivity.kt
+++ app/src/main/java/testing/belajar/belajarget/activity/GETActivity.kt
@@ -2,14 +2,18 @@ package testing.belajar.belajarget.activity
 
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.androidnetworking.AndroidNetworking
 import com.androidnetworking.common.Priority
 import com.androidnetworking.error.ANError
+import com.androidnetworking.interceptors.HttpLoggingInterceptor
 import com.androidnetworking.interfaces.JSONArrayRequestListener
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 import testing.belajar.belajarget.R
+
 
 class GETActivity : AppCompatActivity() {
 
@@ -22,6 +26,9 @@ class GETActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get)
 
+        AndroidNetworking.initialize(applicationContext)
+        AndroidNetworking.enableLogging(HttpLoggingInterceptor.Level.BODY)
+
         tvUserId = findViewById(R.id.tvUserId)
         tvId = findViewById(R.id.tvId)
         tvTitle = findViewById(R.id.tvTitle)
@@ -30,30 +37,29 @@ class GETActivity : AppCompatActivity() {
         getArray()
     }
 
-    fun getArray(){
+    private fun getArray(){
         AndroidNetworking.get("https://jsonplaceholder.typicode.com/posts")
             .setTag("GET")
             .setPriority(Priority.MEDIUM)
             .build()
             .getAsJSONArray(object : JSONArrayRequestListener{
-                override fun onResponse(response: JSONArray?) {
-                    val jArray = JSONArray(response.toString())
-                    for (i in 0 until jArray.length()) {
+                override fun onResponse(response: JSONArray) {
+                    Toast.makeText(this@GETActivity, "Berhasil", Toast.LENGTH_LONG).show()
                     try {
-                        val oneObject = jArray.getJSONObject(i)
-
-                        tvUserId.text = oneObject.getInt("userId").toString()
-                        tvId.text = oneObject.getInt("id").toString()
-                        tvTitle.text = oneObject.getString("title")
-                        tvBody.text = oneObject.getString("body")
+                        for (i in 0 until response.length()) {
+                            val test = response.getJSONObject(i).getInt("userId")
+                            val test1 = response.getJSONObject(i).getInt("id")
+                            tvUserId.text = test.toString()
+                            tvId.text = test1.toString()
+                        }
                     }
                     catch (e : JSONException) {
-                            e.printStackTrace()
-                        }
+                        e.printStackTrace()
                     }
                 }
 
                 override fun onError(anError: ANError?) {
+                    Toast.makeText(this@GETActivity, "Gagal!", Toast.LENGTH_LONG).show()
                 }
 
             })
